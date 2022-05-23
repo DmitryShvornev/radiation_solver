355ce498ed7e44151e79a9e839a1dc9659fb555c
shvor <shvornev.dmitry@yandex.ru>
2022-05-24 00:40:37 +0300

    [ADD] Создание локальной СЛАУ для элемента

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+import math
 class Node:
 
     def __init__(self, x, y, z):
@@ -7,6 +8,9 @@ class Node:
 
     def __str__(self):
         return f"{[self.x, self.y, self.z]}"
+
+    def get_distance(self, node):
+        return math.sqrt((self.x - node.x)**2 + (self.y - node.y)**2 + (self.z - node.z)**2)
 
 
 class Element:
@@ -25,3 +29,34 @@ class Element:
         z = (self.i_node.z + self.j_node.z + self.k_node.z) / 3
         center = Node(x, y, z)
         return center
+
+    def is_equal_to(self, element):
+        cond_x = self.i_node.x == element.i_node.x and self.j_node.x == element.j_node.x and self.k_node.x == element.k_node.x
+        cond_y = self.i_node.y == element.i_node.y and self.j_node.y == element.j_node.y and self.k_node.y == element.k_node.y
+        cond_z = self.i_node.z == element.i_node.z and self.j_node.z == element.j_node.z and self.k_node.z == element.k_node.z
+        return cond_x and cond_y and cond_z
+
+    def get_square(self):
+        Xi = self.i_node.x
+        Yi = self.i_node.y
+        Xj = self.j_node.x
+        Yj = self.j_node.y
+        Xk = self.k_node.x
+        Yk = self.k_node.y
+        return abs((Xj - Xi) * (Yk - Yi) - (Xk - Xi * (Yj - Yi))) / 2
+    
+    def get_normal(self):
+        Xi = self.i_node.x
+        Yi = self.i_node.y
+        Zi = self.i_node.z
+        Xj = self.j_node.x
+        Yj = self.j_node.y
+        Zj = self.j_node.z
+        Xk = self.k_node.x
+        Yk = self.k_node.y
+        Zk = self.j_node.z
+        nx = (Yj - Yi) * (Zk - Zi) - (Zj - Zi) * (Yk - Yi)
+        ny = (Zj - Zi) * (Xk - Xi) - (Xj - Xi) * (Zj - Zi)
+        nz = (Xj - Xi) * (Yk - Yi) - (Yj - Yi) * (Xk - Xi) 
+        normal = Node(nx, ny, nz)
+        return normal
--- solver.py
+++ solver.py
@@ -1,7 +1,28 @@
 import meshio
 from data import Node
 from data import Element
+import numpy as np
+import math
 
+# постоянная Стефана-Больцмана
+SIGMA_0 = 5.67 * 10**(-8)
+
+# излучательная способность
+EPSILON = 0.7
+
+#температура
+THETA = 300
+
+def get_rad_vector(center_left, center_right):
+    rx = center_left.x - center_right.x
+    ry = center_left.y - center_right.y
+    rz = center_left.z - center_right.z
+    r = Node(rx, ry, rx)
+    return r
+
+def get_scalar_comp(vector_left, vector_right):
+    comp = vector_left.x * vector_right.x + vector_left.y * vector_right.y + vector_left.z * vector_right.z
+    return comp
 class Solver:
     
     def __init__(self, mesh_filename):
@@ -23,9 +44,41 @@ class Solver:
             element = Element(*nodes)
             self.mesh_elements.append(element)
 
+    def create_local_matrix(self, element):
+        E = (1 / 12) * element.get_square() * np.ones((3,3))
+        Weight = 0
+        current_center = element.get_center()
+        current_square = element.get_square()
+        for elem in self.mesh_elements:
+            if (not element.is_equal_to(elem)):
+                target_center = elem.get_center()
+                target_square = elem.get_square()
+                r = get_rad_vector(target_center, current_center)
+                k_N = get_scalar_comp(r, target_center)
+                k_M = get_scalar_comp(r, current_center)
+                dist = target_center.get_distance(current_center)
+                Weight += k_M * k_N * target_square / (dist**2)
+        Weight *= current_square / (math.pi * 12)     
+        K = Weight * np.ones((3,3))
+        M = E - K
+        return M
+
+    def create_local_vector(self, element):
+        return (1 / 3) * SIGMA_0 * EPSILON * (THETA**4) * element.get_square() * np.ones((3,1))
+
+    def create_global_matrix():
+        pass
+
+    def create_global_vector():
+        pass
+
+    def solve_global_SLAE():
+        pass
+
+    def print_to_mv2():
+        pass
 
 
-mesh = meshio.read("mesh1.msh")
-print(mesh.points)
-solver = Solver("mesh1.msh")
+
+solver = Solver("mesh_final.msh")
 solver.mesh_preprocessing()
